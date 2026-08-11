e42a986fd82fb664056ed906c05d6aaebd129726
kathleen547 <blueberry8947@gmail.com>
2026-08-11 17:03:51 +0200

    Add Playwright tests for moves and game reset

--- e2e/memory-game.spec.js
+++ e2e/memory-game.spec.js
@@ -25,3 +25,76 @@ test('should flip a card after click', async ({ page }) => {
     await card.click();
     await expect(card).toHaveClass(/flipped/);
 });
+
+test('should increase move counter after two different card clicks', async ({ page }) => {
+    await page.goto('http://127.0.0.1:3000/');
+
+    const cards = page.locator('.card');
+    await expect(cards).toHaveCount(16);
+
+    const counter = page.locator("#moves");
+    await expect(counter).toHaveText("0");
+
+    const firstCard = cards.nth(0);
+    const secondCard = cards.nth(1);
+
+    await firstCard.click();
+    await secondCard.click();
+    
+    await expect(counter).toHaveText("1");
+});
+
+
+test('should reset move counter after restart', async ({ page }) => {
+    await page.goto('http://127.0.0.1:3000/');
+
+    const cards = page.locator('.card');
+    await expect(cards).toHaveCount(16);
+
+    const counter = page.locator("#moves");
+    await expect(counter).toHaveText("0");
+
+    const firstCard = cards.nth(0);
+    const secondCard = cards.nth(1);
+    
+    await firstCard.click();
+    await secondCard.click();
+   
+    await expect(counter).toHaveText("1");
+
+    const reset = page.locator('#restart');
+    await reset.click();
+
+    await expect(counter).toHaveText("0");
+    
+});
+
+
+test('should reset cards after restart', async ({ page }) => {
+    await page.goto('http://127.0.0.1:3000/');
+
+    const cards = page.locator('.card');
+    await expect(cards).toHaveCount(16);
+
+    const counter = page.locator("#moves");
+    await expect(counter).toHaveText("0");
+
+    const firstCard = cards.nth(0);
+    const secondCard = cards.nth(1);
+    
+    await firstCard.click();
+    await secondCard.click();
+
+    await expect(counter).toHaveText("1");
+    const reset = page.locator('#restart');
+    await reset.click();
+
+    await expect(counter).toHaveText("0");
+
+    const flippedCards = page.locator('.cards.flipped');
+    await expect(flippedCards).toHaveCount(0);
+
+    await expect(cards).toHaveCount(16);
+
+
+});
